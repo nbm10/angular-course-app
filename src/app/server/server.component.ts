abed09915e97ca51977664a987b7e15162872a01
nbm10 <neomalefetse@gmail.com>
2017-10-25 15:02:50 +0200

    Second commit

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+  serverId = 10;
+  serverStatus = 'Offline';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
   constructor() { }
 
   ngOnInit() {
